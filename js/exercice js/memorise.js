92513b43681709eb85f089175a206082c992144a
muustapha <moustaf6920@hotmail.fr>
2023-09-21 17:05:02 +0200

    afpa

--- js/exercice js/memorise.js
+++ js/exercice js/memorise.js
@@ -4,7 +4,8 @@ var nbrsDeMauvaiseTentative = "";
 var tempsTentative = "";
 var carte = [];
 var partieEnCours = false;
-var joueurActuel = 1;   
+var joueurActuel = 1; 
+
 const arrayCarte = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 
 function randomCard() {
