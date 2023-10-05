4605ba80eb7078b57c42e1bcd926a6770b03fc32
muustapha <moustaf6920@hotmail.fr>
2023-10-05 22:47:09 +0200

    afpa

--- php/exercice/ville/Dbconnect.class.php
+++ php/exercice/ville/Dbconnect.class.php
@@ -6,38 +6,18 @@ class DbConnect
     /*****************Attributs***************** */
     private static $db;
 
-    /*****************Accesseurs***************** */
-
-    
-    /*****************Constructeur***************** */
-
-    public function __construct(array $options = [])
-    {
-        if (!empty($options)) // empty : renvoi vrai si le tableau est vide
-        {
-            $this->hydrate($options);
-        }
-    }
-    public function hydrate($data)
-    {
-        foreach ($data as $key => $value)
-        {
-            $methode = "set" . ucfirst($key); //ucfirst met la 1ere lettre en majuscule
-            if (is_callable(([$this, $methode]))) // is_callable verifie que la methode existe
-            {
-                $this->$methode($value);
-            }
-        }
-    }
 public static function getDb()
 {
     return self::$db;
 }
 public static function int()
-{
+{ try {
+    self::$db = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
+    self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+}
 
-self::$db = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
-self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
    
-}
---
+++ php/exercice/ville/main.php
@@ -0,0 +1,23 @@
+<?php
+
+function ChargerClasse($ville)
+{
+require $ville.'.Class.php';
+}
+spl_autoload_register('ChargerClasse');
+
+// Instanciation de l'objet Ville$ville
+$ville = new $ville();
+
+// Définition des paramètres de la requête
+$colonnes = ['nomVille', 'codePostal', 'superficie'];
+$conditions = ['superficie->' => '<10000'];
+$orderBy = ['superficie' => 'ASC'];
+
+// Exécution de la requête
+$villes = $ville->select($colonnes, $conditions, $orderBy);
+
+// Affichage des résultats
+foreach ($villes as $ville) {
+    echo $ville->getNomVille() . ' - ' . $ville->getCodePostal() . ' - ' . $ville->getSuperficie() . '<br>';
+}
