f40bdaf1ead0974afa25c89c342858996b959fc7
59011-82-01 <moustaf6920@hotmail.fr>
2023-10-30 11:14:03 +0100

    afpa

--- C#/exercice/exercice1.cs
+++ C#/exercice/exercice1.cs
@@ -1,11 +1,1 @@
-﻿using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Entrez votre nom : ");
-        string nom = Console.ReadLine();
-        Console.WriteLine("Bonjour, " + nom + " !");
-    }
-}
+﻿
