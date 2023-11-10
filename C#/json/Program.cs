8140cca72d6f00d501cb51872f58cbca36d0b64c
59011-82-01 <moustaf6920@hotmail.fr>
2023-11-10 11:34:29 +0100

    afpa

---
+++ C#/json/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Exemple d'utilisation avec une variable simple
+        string contenuVariableSimple = "Contenu de la variable simple";
+        string nomFichierVariableSimple = @"U:\59011-82-01\C#\json\test1.txt";
+
+        GereFichier gereFichier = new GereFichier();
+        gereFichier.EcrireContenuDansFichier(contenuVariableSimple, nomFichierVariableSimple);
+
+        // Exemple d'utilisation avec une variable de type tableau
+        int[] contenuTableau = { 1, 2, 3, 4, 5 };
+        string nomFichierTableau = @"U:\59011-82-01\C#\json\test1.txt";
+
+        gereFichier.EcrireContenuTableauDansFichier(contenuTableau, nomFichierTableau);
+    }
+}
+
+public class GereFichier
+{
+    public void EcrireContenuDansFichier(string contenu, string nomFichier)
+    {
+        using (StreamWriter sw = new StreamWriter(nomFichier))
+        {
+            sw.Write(contenu);
+        }
+    }
+
+    public void EcrireContenuTableauDansFichier(int[] contenu, string nomFichier)
+    {
+        using (StreamWriter sw = new StreamWriter(nomFichier))
+        {
+            foreach (int i in contenu)
+            {
+                sw.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ C#/json/gereFichier.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json
+{
+    using System;
+    using System.IO;
+
+    public class GereFichier
+    {
+        public void EcrireContenuDansFichier(string contenu, string nomFichier)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(nomFichier, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] bytes = System.Text.Encoding.UTF8.GetBytes(contenu);
+                    fileStream.Write(bytes, 0, bytes.Length);
+                }
+
+                Console.WriteLine($"Le contenu a été écrit dans le fichier {nomFichier} avec succès.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erreur lors de l'écriture dans le fichier : {e.Message}");
+            }
+        }
+
+        public void EcrireContenuTableauDansFichier<T>(T[] contenu, string nomFichier)
+        {
+            try
+            {
+                using (FileStream fileStream = new FileStream(nomFichier, FileMode.Create, FileAccess.Write))
+                {
+                    foreach (var item in contenu)
+                    {
+                        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(item.ToString());
+                        fileStream.Write(bytes, 0, bytes.Length);
+                    }
+                }
+
+                Console.WriteLine($"Le contenu du tableau a été écrit dans le fichier {nomFichier} avec succès.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erreur lors de l'écriture dans le fichier : {e.Message}");
+            }
+        }
+        public string LireContenuFichier(string nomFichier)
+        {
+            try
+            {
+                // Vérifier si le fichier existe
+                if (!File.Exists(nomFichier))
+                {
+                    Console.WriteLine($"Le fichier {nomFichier} n'existe pas.");
+                    return null;
+                }
+
+                // Lire le contenu du fichier
+                using (StreamReader reader = new StreamReader(nomFichier))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erreur lors de la lecture du fichier : {e.Message}");
+                return null;
+
+            }
+        }
+    }
+}
