c01323a65e869acd36c67c4f1487e6b77c13076f
MOUS TAF <moustaf6920@hotmail.fr>
2023-12-08 11:35:24 +0100

    WpfDbPersonne

---
+++ C#/WpfDbPersonne/Detail.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfDbPersonne.Models.Data;
+using WpfDbPersonne.Models.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WpfDbPersonne.Models;
+using WpfDbPersonne.Models.Services;
+
+namespace WpfDbPersonne
+{
+    public partial class Detail : Window
+    {
+        PersonneDbContext _context;
+        PersonneService _service;
+
+        public string Mode { get; set; }
+
+        public Detail(Personne p, MainWindow w, string mode)
+        {
+            InitializeComponent();
+            _context = new PersonneDbContext();
+            _service = new PersonneService(_context);
+            Mode = mode;
+            valider.Content = Mode;
+            RemplissageChamp(p);
+        }
+
+        public void RemplissageChamp(Personne p)
+        {
+
+            if (p == null)
+            {
+                return;
+            }
+
+            if (Mode != "Ajouter")
+            {
+                Nom.Text = p.Nom;
+                idPersonne.Content = p.IdPersonne.ToString();
+                Prenom.Text = p.Prenom.ToString();
+                CodePostal.Text = p.CodePostal.ToString();
+                Ville.Text = p.Ville.ToString();
+                Ville.Text = p.Ville;
+            }
+            else
+            {
+                idPersonne.Content = "0";
+            }
+        }
+        private void Click_Valider(object sender, RoutedEventArgs e)
+        {
+            
+            string nom = Nom.Text;
+            string prenom = Prenom.Text;
+            int codePostal = Int32.Parse(CodePostal.Text);
+            string ville = Ville.Text;
+
+            Personne p = new Personne(nom, prenom, codePostal, ville);
+            switch (Mode)
+            {
+                case "Ajouter": _service.AddPersonne(p); break;
+                case "Modifier": _service.UpdatePersonne(p); break;
+                case "Supprimer": _service.DeletePersonne(p); break;
+            }
+            this.Close();
+        }
+
+        private void Click_Annuler(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+    }
+
+}
---
+++ C#/WpfDbPersonne/Models/Data/Personnes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfDbPersonne.Models.Data;
+
+public partial class Personne
+{
+    public uint IdPersonne { get; set; }
+
+    public string? Nom { get; set; }
+
+    public string? Prenom { get; set; }
+
+    public int? CodePostal { get; set; }
+
+    public string Adresse { get; set; } = null!;
+
+    public string Ville { get; set; } = null!;
+
+    public Personne(string nom, string prenom, int codePostal, string adresse, string ville)
+    {    
+        Nom = nom;
+        Prenom = prenom;
+        CodePostal = codePostal;
+        Adresse = adresse;
+        Ville = ville;
+    }
+}
