ee512ebafd16e270590336e45a2549f66f160141
MOUS TAF <moustaf6920@hotmail.fr>
2023-12-11 15:32:17 +0100

    mise a jour DTO

---
+++ C#/WpfPersonne2/Models/Data/Personnes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WpfDbPersonne.Models.Data;
+
+public partial class Personne
+{
+    public uint IdPersonne { get; set; }
+
+    public string? Nom { get; set; }
+
+    public string? Prenom { get; set; }
+
+    public int? CodePostal { get; set; }
+
+    public string Adresse { get; set; } = null!;
+
+    public string Ville { get; set; } = null!;
+
+    public Personne(uint idPersonne, string nom, string prenom, int codePostal, string adresse, string ville)
+    {   
+        IdPersonne = idPersonne;
+        Nom = nom;
+        Prenom = prenom;
+        CodePostal = codePostal;
+        Adresse = adresse;
+        Ville = ville;
+    }
+
+
+    public Personne(string nom, string prenom, int codePostal, string adresse, string ville)
+    {
+        Nom = nom;
+        Prenom = prenom;
+        CodePostal = codePostal;
+        Adresse = adresse;
+        Ville = ville;
+    }
+
+    public Personne()
+    {
+        Nom = null;
+        Prenom = null;
+        CodePostal = null;
+        Adresse = null;
+        Ville = null;
+    }
+
+
+}
---
+++ C#/WpfPersonne2/Models/Dtos/PersonneDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDbPersonne.Models.Dtos
+{
+    public class PersonneDTO
+    {
+
+        public string Nom { get; set; } = null!;
+
+        public string Prenom { get; set; } = null!;
+
+        public int? CodePostal { get; set; }
+
+        public string? Adresse { get; set; }
+
+        public string? Ville { get; set; }
+    }
+
+}
