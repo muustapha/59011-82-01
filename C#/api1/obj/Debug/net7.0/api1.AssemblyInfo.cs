d3c1cc69c904b91556932e2791a97b0cc72c0a8c
MOUS TAF <moustaf6920@hotmail.fr>
2023-12-11 15:42:07 +0100

    mise a jour nom dossier

--- C#/api1/obj/Debug/net7.0/api1.AssemblyInfo.cs
+++ C#/api1/obj/Debug/net7.0/api1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("API1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("api1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("API1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("API1")]
+[assembly: System.Reflection.AssemblyProductAttribute("api1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("api1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
